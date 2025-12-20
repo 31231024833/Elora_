6249053458e7c1bfadab3d03cd5ba85f745118e8
31231024833 <quanghuynh.31231024833@st.ueh.edu.vn>
2025-12-20 23:11:08 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,8 @@ dotenv.config();
 // Kết nối MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/elora_spa', {
+    console.log(process.env.MONGODB_URIS);
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://appuser:appuser@elora.muz6n87.mongodb.net/?appName=ELORA', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
